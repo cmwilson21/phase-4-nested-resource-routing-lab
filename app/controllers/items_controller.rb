8e3b221cc532b6e97871f53e64c5bafa69bf4f2c
Courtney Wilson <cmwilson132@gmail.com>
2021-09-20 10:58:49 -0500

    commit tree 2145d26a493e2d75721a7402c65251e0f1ce3e5d

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,47 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = find_user
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
 
+  def show
+   if item = find_item
+    render json: item
+   else
+    render_not_found_response
+   end
+  end
+
+  def create
+    user = find_user
+    item = user.items.create(item_params)
+    render json: item, status: :created
+  end
+
+
+
+
+
+  private
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def find_item
+    Item.find(params[:id])
+  end
+
+  def find_user
+    User.find(params[:user_id])
+  end
+
+  def render_not_found_response
+    render json: { error: "Review not found" }, status: :not_found
+  end
 end
